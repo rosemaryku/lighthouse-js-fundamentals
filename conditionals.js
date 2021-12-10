28838cd69cddf216f5e0a53a0cec846212b4ad68
Rosemary Ku <rosemary.ku@hotmail.com>
2021-12-10 16:20:43 -0500

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,4 +1,4 @@
-const raining = true;
+const raining = false;
 const cold = false;
 
 if (raining) {
@@ -27,4 +27,4 @@ if (temperature < -40 || temperature > 40) {
 
 if (!raining) {
   console.log("Leave your umbrella at home!");
-}
+} 
